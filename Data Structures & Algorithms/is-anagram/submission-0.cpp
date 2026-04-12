858af17101a830e1688d257b289ece482cf5f5ec
sanhaw154 <51456885+sanhaw154@users.noreply.github.com>
2026-04-12 16:32:50 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26] = {0};
+
+        for(auto& c : s)
+        {
+                freq[c - 'a']++;
+        }
+        for(auto& c : t)
+        {
+                freq[c - 'a']--;
+        }
+        for(int i = 0; i < 26; i++)
+        {
+            if(freq[i]) return false;
+        }
+        return true;
+    }
+};
